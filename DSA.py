48d0ba1eeb27438c99a50d113ac8f6fc70ea54e8
tmrupp <tmrupp@umich.edu>
2021-04-03 09:34:08 -0700

    proof of discrete log equality

--- DSA.py
+++ DSA.py
@@ -54,19 +54,61 @@ def DLProof (key):
     proof that given y = g^x, x is known
     '''
     x, q, p, g, y = decomposeKey(key)
-    v = Crypto.Random.random.randint(0, int(q))
+    v = Crypto.Random.random.randint(0, int(p))
     t = pow(g, v, p)
     c = sha_crypto_ints([g, y, t])
     r = (v - (c * int(x))) % int(p-1)
-
     return (t, r)
 
-def DLVerify (publicKey, proof):
+def DLVerify (publicKey, proof, cin=None):
     q, p, g, y = decomposePublicKey(publicKey)
     t, r = proof
-    c = sha_crypto_ints([g, y, t])
+    c = sha_crypto_ints([g, y, t]) if cin is None else cin
     return t == ((pow(g, r, p) * pow (y, c, p)) % p)
 
-myProof = FSProof(myKey)
-print("verified?", FSVerify(myKey.publickey(), myProof))
+def DLEqualityProof (keyA, keyB):
+    '''
+    knowledge of the equality of two discrete logs
+    log_ag(ay) == log_bg(by), ay & by are public keys, ag & bg are generators
+    '''
+    ax, q, p, ag, ay = decomposeKey(keyA)
+    bx, _, _, bg, by = decomposeKey(keyB)
+    v = Crypto.Random.random.randint(0, int(p))
+    ts = (pow(ag, v, p), pow(bg, v, p))
+    c = sha_crypto_ints([ag, ay, ts[0], bg, by, ts[1]])
+    r = (v - (c * int(x))) % int(p-1)
+    return (ts, r)
+
+def DLEqualityVerify (publicKeyA, publicKeyB, proof):
+    (ta, tb), r = proof
+    q, p, ag, ay = decomposePublicKey(publicKeyA)
+    _, _, bg, by = decomposePublicKey(publicKeyB)
+    c = sha_crypto_ints([ag, ay, ta, bg, by, tb])
+    return DLVerify(publicKeyA, (ta, r), c) and DLVerify(publicKeyB, (tb, r), c)
+
+def keyFromNewGenerator (key):
+    '''
+    creates a new key with a new generator, with the same private key
+    '''
+    x, q, p, g, y = decomposeKey(key)
+    ng = 1
+    while (ng == g or ng == 1):
+        h = Crypto.Random.random.randint(2, int(p)-2)
+        ng = pow(h, (int(p)-1)//int(q), int(p))
+
+    ny = pow(ng, int(x), int(p))
+    print ("ng=", ng, "ny=", ny, "ng^x=", pow(ng, int(x), int(p)))
+
+    return DSA.construct((ny, ng, p, q, x))
+
+
+myProof = DLProof(myKey)
+print("verified?", DLVerify(myKey.publickey(), myProof))
+newKey = keyFromNewGenerator(myKey)
+myEqProof = DLEqualityProof(myKey, newKey)
+print("equality verified?", DLEqualityVerify(myKey.publickey(), newKey.publickey(), myEqProof))
+
+
+
+
 
